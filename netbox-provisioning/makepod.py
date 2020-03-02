8415d669b2370560fec62e653935d1f667d6e538
Antoine Delannoy <ady@cloud-temple.com>
2020-03-02 15:31:04 +0100

    Update makepod.py. functional

--- netbox-provisioning/makepod.py
+++ netbox-provisioning/makepod.py
@@ -23,10 +23,11 @@ class POD_SPECS:
     SITE_NAME = "site-pod{pod_id:02d}"
     DEVICE_ROLE = "lab-pod"
     MODEL = {
-        "rtrs": [
+        "devices": [
             {
                 "id": 1,
                 "device_type": 0,
+                "loopback": "lo0",
                 "name": "vqfx 1",
                 "tags": ["router", "border-router"],
                 "interfaces": [
@@ -51,6 +52,7 @@ class POD_SPECS:
             {
                 "id": 2,
                 "device_type": 0,
+                "loopback": "lo0",
                 "name": "vqfx 2",
                 "tags": ["router"],
                 "interfaces": [
@@ -67,6 +69,7 @@ class POD_SPECS:
             {
                 "id": 3,
                 "device_type": 1,
+                "loopback": "lo0",
                 "tags": ["router", "border-router"],
                 "interfaces": [
                     {
@@ -90,6 +93,7 @@ class POD_SPECS:
             {
                 "id": 4,
                 "device_type": 1,
+                "loopback": "lo0",
                 "tags": ["router"],
                 "interfaces": [
                     {
@@ -104,18 +108,59 @@ class POD_SPECS:
             },
         ],
         "connections": [
-            {"a": 1, "z": 7, "prefixv4": "10.{pod_id}.0.0/31", "prefixv6": "fe80:{pod_id}::0/127"},
-            {"a": 2, "z": 6, "prefixv4": "10.{pod_id}.0.2/31", "prefixv6": "fe80:{pod_id}::2/127"},
-            {"a": 5, "z": 11, "prefixv4": "10.{pod_id}.0.4/31", "prefixv6": "fe80:{pod_id}::4/127"},
-            {"a": 8, "z": 12, "prefixv4": "10.{pod_id}.0.6/31", "prefixv6": "fe80:{pod_id}::6/127"}
+            {
+                "a": 1,
+                "z": 7,
+                "prefixv4": "10.{pod_id}.0.0/31",
+                "prefixv6": "fe80:{pod_id}::0/127"
+            },
+            {
+                "a": 2,
+                "z": 6,
+                "prefixv4": "10.{pod_id}.0.2/31",
+                "prefixv6": "fe80:{pod_id}::2/127"
+            },
+            {
+                "a": 5,
+                "z": 11,
+                "prefixv4": "10.{pod_id}.0.4/31",
+                "prefixv6": "fe80:{pod_id}::4/127"
+            },
+            {
+                "a": 8,
+                "z": 12,
+                "prefixv4": "10.{pod_id}.0.6/31",
+                "prefixv6": "fe80:{pod_id}::6/127"
+            }
         ],
         "uplinks": [
-            {"subnet": "10.{pod_id}.99.0/31", "interface": 3, "tags": ["tier-1", "Acorus"]},
-            {"subnet": "10.{pod_id}.99.2/31", "interface": 4, "tags": ["tier-1", "Acorus"]},
-            {"subnet": "10.{pod_id}.99.4/31", "interface": 9, "tags": ["tier-1", "Cloud Temple"]},
-            {"subnet": "10.{pod_id}.99.6/31", "interface": 10, "tags": ["tier-1", "Cloud Temple"]}
+            {
+                "prefixv4": "10.{pod_id}.99.0/31",
+                "prefixv6": "fc00:{pod_id}::99:0/127",
+                "interface": 3,
+                "tags": ["tier-1", "Acorus"]
+            },
+            {
+                "prefixv4": "10.{pod_id}.99.2/31",
+                "prefixv6": "fc00:{pod_id}::99:2/127",
+                "interface": 4,
+                "tags": ["tier-1", "Acorus"]
+            },
+            {
+                "prefixv4": "10.{pod_id}.99.4/31",
+                "prefixv6": "fc00:{pod_id}::99:4/127",
+                "interface": 9,
+                "tags": ["tier-1", "Cloud Temple"]
+            },
+            {
+                "prefixv4": "10.{pod_id}.99.6/31",
+                "prefixv6": "fc00:{pod_id}::99:6/127",
+                "interface": 10,
+                "tags": ["tier-1", "Cloud Temple"]
+            }
         ]
     }
+
 
 def parse_cli_args(script_args):
     parser = argparse.ArgumentParser()
@@ -130,36 +175,6 @@ def parse_cli_args(script_args):
     )
 
     return parser.parse_args(script_args)
-
-
-def setup_device_role(api_connector, spec, color):
-    device_role = api_connector.dcim.device_roles.get(name=spec.DEVICE_ROLE)
-    if device_role is None:
-        device_role = api_connector.dcim.device_roles.create(
-            name=spec.DEVICE_ROLE,
-            slug=spec.DEVICE_ROLE,
-            color=color,
-        )
-    return device_role
-
-
-def setup_tenant(api_connector, tenant_name):
-    tenant = api_connector.tenancy.tenants.get(name=tenant_name)
-    if tenant is None:
-        tenant = api_connector.tenancy.tenants.create(
-            name=tenant_name,
-            slug=tenant_name
-        )
-    return tenant
-
-
-def setup_site(api_connector, slug, tenant, asn):
-    return api_connector.dcim.sites.create(
-        name=slug,
-        slug=slug,
-        asn=asn,
-        tenant=tenant.id,
-    )
 
 
 def setup_rack(api_connector, pod_id, site):
@@ -195,46 +210,6 @@ def setup_device_type(api_connector, spec, dt_id):
     return device_type
 
 
-def setup_device(api_connector, name, device_role, device_type, tenant, site, tags=[]):
-    return api_connector.dcim.devices.create(
-        name=name,
-        device_role=device_role.id,
-        device_type=device_type.id,
-        tenant=tenant.id,
-        site=site.id,
-        tags=tags
-    )
-
-
-def setup_interface(api_connector, device, name):
-    return api_connector.dcim.interfaces.create(
-        name=name,
-        device=device.id
-    )
-
-
-def setup_loopback(api_connector, device, address):
-    interface = api_connector.dcim.interfaces.create(
-        name="lo0",
-        device=device.id,
-        type=0
-    )
-    ip = api_connector.ipam.ip_addresses.create(
-        address=address,
-        interface=interface.id
-    )
-    device.primary_ip4 = ip
-    device.save()
-
-
-def setup_connection(api_connector, a, z):
-    api_connector.dcim.cables.create(
-        termination_a_type="dcim.interface",
-        termination_a_id=a.id,
-        termination_b_type="dcim.interface",
-        termination_b_id=z.id
-    )
-
 def make_pod(pod_id, api_connector):
     # Setup regular routeurs
     SPEC = POD_SPECS
@@ -244,67 +219,85 @@ def make_pod(pod_id, api_connector):
         "interfaces": {}
     }
 
-    tenant = setup_tenant(
-        api_connector,
-        tenant_name=SPEC.TENANT_NAME.format(pod_id=pod_id)
-    )
+    # Setup tenant
+    tenant_name = SPEC.TENANT_NAME.format(pod_id=pod_id)
+    tenant = api_connector.tenancy.tenants.get(name=tenant_name)
+    if tenant is None:
+        tenant = api_connector.tenancy.tenants.create(
+            name=tenant_name,
+            slug=tenant_name
+        )
 
-    device_role = setup_device_role(
-        api_connector,
-        spec=SPEC,
-        color="f44336"
-    )
+    # Setup device role
+    device_role = api_connector.dcim.device_roles.get(name=SPEC.DEVICE_ROLE)
+    if device_role is None:
+        device_role = api_connector.dcim.device_roles.create(
+            name=SPEC.DEVICE_ROLE,
+            slug=SPEC.DEVICE_ROLE,
+            color="f44336",
+        )
 
+    # Setup site
     site = api_connector.dcim.sites.create(
         name=SPEC.SITE_NAME.format(pod_id=pod_id),
         slug=SPEC.SITE_NAME.format(pod_id=pod_id),
         asn="655{pod_id:02d}".format(pod_id=pod_id),
         description="The site of the {}th pod.".format(pod_id),
+        physical_address="2 Rue Scribe, Paris, France",
         tenant=tenant.id,
     )
 
-    for rtr in model["rtrs"]:
-        assert rtr['id'] not in pod_refs["devices"]
+    for dev in model["devices"]:
+        # Avoid looping on the same objects - dev
+        assert dev['id'] not in pod_refs["devices"]
 
         device_type = setup_device_type(
             api_connector,
             spec=SPEC,
-            dt_id=rtr["device_type"]
+            dt_id=dev["device_type"]
         )
 
-        # make_device
-        device = setup_device(
-            api_connector,
-            name="{}{}".format(device_type.model, rtr['id']),
-            device_role=device_role,
-            device_type=device_type,
-            tenant=tenant,
-            site=site,
-            tags=rtr['tags']
+        # Setup device
+        device = api_connector.dcim.devices.create(
+            name="{}{}".format(device_type.model, dev['id']),
+            device_role=device_role.id,
+            device_type=device_type.id,
+            tenant=tenant.id,
+            site=site.id,
+            tags=dev['tags']
         )
 
+        # Setup loopback interface
         interface = api_connector.dcim.interfaces.create(
-            name="lo0",
+            name=dev["loopback"],
             device=device.id,
             type=0
         )
+        # Attach IP to loopback
         ipv4 = api_connector.ipam.ip_addresses.create(
-            address=SPEC.LOOPBACKv4.format(pod_id=pod_id, rtr_id=rtr['id']),
+            address=SPEC.LOOPBACKv4.format(pod_id=pod_id, rtr_id=dev['id']),
             interface=interface.id
         )
+        # Attach IP to loopback
         ipv6 = api_connector.ipam.ip_addresses.create(
-            address=SPEC.LOOPBACKv6.format(pod_id=pod_id, rtr_id=rtr['id']),
+            address=SPEC.LOOPBACKv6.format(pod_id=pod_id, rtr_id=dev['id']),
             interface=interface.id
         )
         device.primary_ip4 = ipv4
         device.primary_ip6 = ipv6
         device.save()
 
-        pod_refs["devices"][rtr['id']] = device
-        for intf in rtr['interfaces']:
-            # make_interface
+        # Load device in reference
+        pod_refs["devices"][dev['id']] = device
+
+        # Setup ico interfaces
+        for intf in dev['interfaces']:
             assert intf['id'] not in pod_refs['interfaces']
-            interface = setup_interface(api_connector, device, intf["name"])
+            interface = api_connector.dcim.interfaces.create(
+                name=intf["name"],
+                device=device.id
+            )
+            # Load interface in references
             pod_refs["interfaces"][intf['id']] = interface
 
     for co in model["connections"]:
@@ -328,29 +321,63 @@ def make_pod(pod_id, api_connector):
                 )
 
         # Connect the interfaces
-        setup_connection(api_connector, a, z)
+        api_connector.dcim.cables.create(
+            termination_a_type="dcim.interface",
+            termination_a_id=a.id,
+            termination_b_type="dcim.interface",
+            termination_b_id=z.id
+        )
+
     return pod_refs
+
 
 def main():
     args = vars(parse_cli_args(sys.argv[1:]))
+    # Netbox API connector
     api_connector = pynetbox.api(
         url=args['netbox_url'],
         token=args['netbox_token']
     )
 
     pods_refs = make_pod(args['id'], api_connector)
+
     for uplink in POD_SPECS.MODEL["uplinks"]:
-        subnet = uplink['subnet'].format(pod_id=args['id'])
+        subnet = uplink['prefixv4'].format(pod_id=args['id'])
         api_connector.ipam.prefixes.create(
             prefix=subnet,
-            description="Tier1-ICO"
+            description="Tier1-ICO-v4"
         )
         api_connector.ipam.ip_addresses.create(
-            address="{}/32".format(str(list(ipaddress.ip_network(subnet))[1])),
+            address=(
+                "{}/32"
+                .format(str(list(ipaddress.ip_network(subnet))[1]))
+            ),
             interface=pods_refs["interfaces"][uplink["interface"]].id,
         )
         api_connector.ipam.ip_addresses.create(
-            address="{}/32".format(str(list(ipaddress.ip_network(subnet))[0])),
+            address=(
+                "{}/32"
+                .format(str(list(ipaddress.ip_network(subnet))[0]))
+            ),
+            tags=uplink["tags"]
+        )
+        subnet = uplink['prefixv6'].format(pod_id=args['id'])
+        api_connector.ipam.prefixes.create(
+            prefix=subnet,
+            description="Tier1-ICO-v6"
+        )
+        api_connector.ipam.ip_addresses.create(
+            address=(
+                "{}/127"
+                .format(str(list(ipaddress.ip_network(subnet))[1]))
+            ),
+            interface=pods_refs["interfaces"][uplink["interface"]].id,
+        )
+        api_connector.ipam.ip_addresses.create(
+            address=(
+                "{}/127"
+                .format(str(list(ipaddress.ip_network(subnet))[0]))
+            ),
             tags=uplink["tags"]
         )
 
