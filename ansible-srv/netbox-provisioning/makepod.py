efebca992b0e3896033c467a398898160a897e12
Antoine Delannoy <ady@cloud-temple.com>
2020-03-04 16:06:12 +0100

    update makepod.py

--- ansible-srv/netbox-provisioning/makepod.py
+++ ansible-srv/netbox-provisioning/makepod.py
@@ -11,22 +11,36 @@ class POD_SPECS:
     DEVICE_TYPES = [
         {
             "model": "vqfx",
-            "manufacturer": "Juniper Networks"
+            "manufacturer": "juniper"
         },
         {
             "model": "veos",
-            "manufacturer": "Arista Networks"
+            "manufacturer": "arista"
+        },
+    ]
+    CIRCUIT_PROVIDERS = [
+        {
+            "name": "Acorus",
+            "slug": "acorus",
+            "asn": 35280,
+        },
+        {
+            "name": "Cloud-Temple",
+            "slug": "cloud-temple",
+            "asn": 33930,
         },
     ]
     LOOPBACKv4 = "10.{pod_id}.200.{rtr_id}/32"
-    LOOPBACKv6 = "fddd:{pod_id}::{rtr_id}/128"
+    LOOPBACKv6 = "fd00:{pod_id}::{rtr_id}/128"
     SITE_NAME = "site-pod{pod_id:02d}"
     DEVICE_ROLE = "lab-pod"
+    RACK = "rack-pod{pod_id:02d}"
     MODEL = {
         "devices": [
             {
                 "id": 1,
                 "device_type": 0,
+                "mgmt": "192.168.100.20",
                 "loopback": "lo0",
                 "name": "vqfx 1",
                 "tags": ["router", "border-router"],
@@ -41,17 +55,22 @@ class POD_SPECS:
                     },
                     {
                         "name": "xe-0/0/2",
-                        "id": 3
+                        "id": 3,
+                        "tags": ["transit", 'acorus'],
+                        "provider": 0
                     },
                     {
                         "name": "xe-0/0/3",
-                        "id": 4
+                        "id": 4,
+                        "tags": ["transit", 'acorus'],
+                        "provider": 0
                     },
                 ]
             },
             {
                 "id": 2,
                 "device_type": 0,
+                "mgmt": "192.168.100.21",
                 "loopback": "lo0",
                 "name": "vqfx 2",
                 "tags": ["router"],
@@ -69,6 +88,7 @@ class POD_SPECS:
             {
                 "id": 3,
                 "device_type": 1,
+                "mgmt": "192.168.100.22",
                 "loopback": "lo0",
                 "tags": ["router", "border-router"],
                 "interfaces": [
@@ -82,17 +102,22 @@ class POD_SPECS:
                     },
                     {
                         "name": "et3",
-                        "id": 9
+                        "id": 9,
+                        "tags": ["transit", 'cloud-temple'],
+                        "provider": 1
                     },
                     {
                         "name": "et4",
-                        "id": 10
+                        "id": 10,
+                        "tags": ["transit", 'cloud-temple'],
+                        "provider": 1
                     },
                 ]
             },
             {
                 "id": 4,
                 "device_type": 1,
+                "mgmt": "192.168.100.23",
                 "loopback": "lo0",
                 "tags": ["router"],
                 "interfaces": [
@@ -112,49 +137,49 @@ class POD_SPECS:
                 "a": 1,
                 "z": 7,
                 "prefixv4": "10.{pod_id}.0.0/31",
-                "prefixv6": "fe80:{pod_id}::0/127"
+                "prefixv6": "fd00:{pod_id}:1::0/127"
             },
             {
                 "a": 2,
                 "z": 6,
                 "prefixv4": "10.{pod_id}.0.2/31",
-                "prefixv6": "fe80:{pod_id}::2/127"
+                "prefixv6": "fd00:{pod_id}:1::2/127"
             },
             {
                 "a": 5,
                 "z": 11,
                 "prefixv4": "10.{pod_id}.0.4/31",
-                "prefixv6": "fe80:{pod_id}::4/127"
+                "prefixv6": "fd00:{pod_id}:1::4/127"
             },
             {
                 "a": 8,
                 "z": 12,
                 "prefixv4": "10.{pod_id}.0.6/31",
-                "prefixv6": "fe80:{pod_id}::6/127"
+                "prefixv6": "fd00:{pod_id}:1::6/127"
             }
         ],
         "uplinks": [
             {
                 "prefixv4": "10.{pod_id}.99.0/31",
-                "prefixv6": "fc00:{pod_id}::99:0/127",
+                "prefixv6": "fdaa:{pod_id}::0/127",
                 "interface": 3,
                 "tags": ["tier-1", "Acorus"]
             },
             {
                 "prefixv4": "10.{pod_id}.99.2/31",
-                "prefixv6": "fc00:{pod_id}::99:2/127",
+                "prefixv6": "fdaa:{pod_id}::2/127",
                 "interface": 4,
                 "tags": ["tier-1", "Acorus"]
             },
             {
                 "prefixv4": "10.{pod_id}.99.4/31",
-                "prefixv6": "fc00:{pod_id}::99:4/127",
+                "prefixv6": "fdaa:{pod_id}::4/127",
                 "interface": 9,
                 "tags": ["tier-1", "Cloud Temple"]
             },
             {
                 "prefixv4": "10.{pod_id}.99.6/31",
-                "prefixv6": "fc00:{pod_id}::99:6/127",
+                "prefixv6": "fdaa:{pod_id}::6/127",
                 "interface": 10,
                 "tags": ["tier-1", "Cloud Temple"]
             }
@@ -177,94 +202,94 @@ def parse_cli_args(script_args):
     return parser.parse_args(script_args)
 
 
-def setup_rack(api_connector, pod_id, site):
-    return api_connector.dcim.racks.create(
-        name=POD_SPECS.RACK.format(pod_id=pod_id),
-        site=site.id
-    )
-
-
-def setup_device_type(api_connector, spec, dt_id):
-    mnf_name = spec.DEVICE_TYPES[dt_id]["manufacturer"]
-    manufacturer = api_connector.dcim.manufacturers.get(
-        name=mnf_name,
-    )
-    if manufacturer is None:
-        manufacturer = api_connector.dcim.manufacturers.create(
-            name=mnf_name,
-            slug=mnf_name.replace(' ', '_')
-        )
-
-    model = spec.DEVICE_TYPES[dt_id]["model"]
-
-    device_type = api_connector.dcim.device_types.get(
-        slug=model
-    )
-    if device_type is None:
-        device_type = api_connector.dcim.device_types.create(
-            manufacturer=manufacturer.id,
-            model=model,
-            slug=model
-        )
-
-    return device_type
-
 
 def make_pod(pod_id, api_connector):
     # Setup regular routeurs
     SPEC = POD_SPECS
     model = SPEC.MODEL
-    pod_refs = {
+    index = {
         "devices": {},
-        "interfaces": {}
-    }
-
-    # Setup tenant
-    tenant_name = SPEC.TENANT_NAME.format(pod_id=pod_id)
-    tenant = api_connector.tenancy.tenants.get(name=tenant_name)
-    if tenant is None:
-        tenant = api_connector.tenancy.tenants.create(
-            name=tenant_name,
-            slug=tenant_name
-        )
-
-    # Setup device role
-    device_role = api_connector.dcim.device_roles.get(name=SPEC.DEVICE_ROLE)
-    if device_role is None:
-        device_role = api_connector.dcim.device_roles.create(
+        "interfaces": {},
+        "tenant": api_connector.tenancy.tenants.create(
+            name=SPEC.TENANT_NAME.format(pod_id=pod_id),
+            slug=SPEC.TENANT_NAME.format(pod_id=pod_id)
+        ),
+        "device_role": api_connector.dcim.device_roles.create(
             name=SPEC.DEVICE_ROLE,
             slug=SPEC.DEVICE_ROLE,
             color="f44336",
-        )
+        ),
+        "circuit_type": api_connector.circuits.circuit_types.create(
+            name="transit",
+            slug="transit",
+        ),
+        "device_types": [],
+        "manufacturer": {}
+    }
 
-    # Setup site
-    site = api_connector.dcim.sites.create(
+    index['site'] = api_connector.dcim.sites.create(
         name=SPEC.SITE_NAME.format(pod_id=pod_id),
         slug=SPEC.SITE_NAME.format(pod_id=pod_id),
         asn="655{pod_id:02d}".format(pod_id=pod_id),
         description="The site of the {}th pod.".format(pod_id),
         physical_address="2 Rue Scribe, Paris, France",
-        tenant=tenant.id,
+        tenant=index['tenant'].id,
     )
 
+    index['rack'] = api_connector.dcim.racks.create(
+        name=SPEC.RACK.format(pod_id=pod_id),
+        site=index["site"].id
+    )
+
+    index['circuit_providers'] = []
+    for prov in SPEC.CIRCUIT_PROVIDERS:
+        index['circuit_providers'].append(
+            api_connector.circuits.providers.create(
+                prov
+            )
+        )
+
+    for dt in SPEC.DEVICE_TYPES:
+        mnf_name = dt["manufacturer"]
+        manufacturer = index['manufacturer'].get(mnf_name)
+        if not manufacturer:
+            manufacturer = api_connector.dcim.manufacturers.create(
+                name=mnf_name,
+                slug=mnf_name.replace(' ', '_')
+            )
+            index['manufacturer'][mnf_name] = manufacturer
+
+        device_model = dt["model"]
+        index['device_types'].append(
+            api_connector.dcim.device_types.create(
+                manufacturer=manufacturer.id,
+                model=device_model,
+                slug=device_model
+            )
+        )
+
+    i = 0
     for dev in model["devices"]:
         # Avoid looping on the same objects - dev
-        assert dev['id'] not in pod_refs["devices"]
-
-        device_type = setup_device_type(
-            api_connector,
-            spec=SPEC,
-            dt_id=dev["device_type"]
-        )
+        assert dev['id'] not in index["devices"]
 
         # Setup device
         device = api_connector.dcim.devices.create(
-            name="{}{}".format(device_type.model, dev['id']),
-            device_role=device_role.id,
-            device_type=device_type.id,
-            tenant=tenant.id,
-            site=site.id,
+            name="{}{}".format(index['device_types'][dev['device_type']].model, dev['id']),
+            device_role=index['device_role'].id,
+            device_type=index['device_types'][dev['device_type']].id,
+            tenant=index['tenant'].id,
+            rack=index['rack'].id,
+            position=int(dev['id']) + 1,
+            face=0,
+            site=index['site'].id,
             tags=dev['tags']
+        )
+
+        mgmt = api_connector.dcim.interfaces.create(
+            name="mgmt",
+            device=device.id,
+            type=0
         )
 
         # Setup loopback interface
@@ -272,6 +297,12 @@ def make_pod(pod_id, api_connector):
             name=dev["loopback"],
             device=device.id,
             type=0
+        )
+
+        # Attach IP to loopback
+        mgmt_ip = api_connector.ipam.ip_addresses.create(
+            address=dev["mgmt"],
+            interface=mgmt.id
         )
         # Attach IP to loopback
         ipv4 = api_connector.ipam.ip_addresses.create(
@@ -288,21 +319,44 @@ def make_pod(pod_id, api_connector):
         device.save()
 
         # Load device in reference
-        pod_refs["devices"][dev['id']] = device
+        index["devices"][dev['id']] = device
 
         # Setup ico interfaces
         for intf in dev['interfaces']:
-            assert intf['id'] not in pod_refs['interfaces']
+            assert intf['id'] not in index['interfaces']
+            tags = intf.get('tags', [])
             interface = api_connector.dcim.interfaces.create(
                 name=intf["name"],
-                device=device.id
+                device=device.id,
+                type=1200,
+                tags=tags
             )
+            if "transit" in tags:
+                c = api_connector.circuits.circuits.create(
+                    cid="ID-{}".format(i),
+                    provider=index['circuit_providers'][intf['provider']].id,
+                    type=index['circuit_type'].id,
+                )
+                ct = api_connector.circuits.circuit_terminations.create(
+                    circuit=c.id,
+                    term_side="A",
+                    port_speed=10000000,
+                    site=index['site'].id,
+                )
+                api_connector.dcim.cables.create(
+                    termination_a_type="circuits.circuittermination",
+                    termination_a_id=ct.id,
+                    termination_b_type="dcim.interface",
+                    termination_b_id=interface.id
+                )
+
+                i += 1
             # Load interface in references
-            pod_refs["interfaces"][intf['id']] = interface
+            index["interfaces"][intf['id']] = interface
 
     for co in model["connections"]:
-        a = pod_refs["interfaces"][co["a"]]
-        z = pod_refs["interfaces"][co["z"]]
+        a = index["interfaces"][co["a"]]
+        z = index["interfaces"][co["z"]]
         ends = [a, z]
         prefix4 = co["prefixv4"].format(pod_id=pod_id)
         prefix6 = co["prefixv6"].format(pod_id=pod_id)
@@ -328,7 +382,7 @@ def make_pod(pod_id, api_connector):
             termination_b_id=z.id
         )
 
-    return pod_refs
+    return index
 
 
 def main():
